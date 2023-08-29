fb215aa3286051bb23fd8be532e606959ea23598
edouard <edouard.laine2@gmail.com>
2023-08-29 23:48:08 +0200

    Commit 8:(correction)
    -REAMDME.md update (still working on it)

--- src/entity/hiddenEntityColumns/user.hiddenEntityColumns.ts
+++
@@ -1,9 +0,0 @@
-import {Base} from "../base/base";
-import {Column} from "typeorm";
-
-export class UserHiddenEntityColumns extends Base{
-
-    @Column({nullable: false, type: "varchar", default: "User"})
-    role : string
-
-}
